6a9f2a42052d9a306109fcdabd1c9227b24112fb
Andrey Shakhmin <andrey.shakhmin@gmail.com>
2016-02-04 18:52:43 +0800

    Added some common constants

---
+++ index.js
@@ -0,0 +1,158 @@
+
+export const FillType = {
+    Solid: 0,
+    Gradient: 1,
+    Pattern: 4,
+    Noise: 5
+};
+
+export const GradientType = {
+    Linear: 0,
+    Radial: 1,
+    Circular: 2
+};
+
+export const PatternFillType = {
+    Tile: 0,
+    Fill: 1
+};
+
+export const NoiseFillType = {
+    Original: 0,
+    Black: 1,
+    White: 2,
+    Color: 3
+};
+
+
+// _MSStyleBorderOptions
+export const BorderLineCapsStyle = {
+    Butt: 0,
+    Round: 1,
+    Square: 2
+};
+
+export const BorderLineJoinStyle = {
+    Miter: 0,
+    Round: 1,
+    Bevel: 2
+};
+
+export const LineDecorationType = {
+    None: 0,
+    OpenedArrow: 1,
+    ClosedArrow: 2,
+    Bar: 3
+};
+
+// _MSStyleBlur
+export const BlurType = {
+    GaussianBlur: 0,
+    MotionBlur: 1,
+    ZoomBlur: 2,
+    BackgroundBlur: 3
+};
+
+export const BorderPosition = {
+    Center: 0,
+    Inside: 1,
+    Outside: 2,
+    Both: 3
+};
+
+export const MaskMode = {
+    Outline: 0,
+    Alpha: 1
+};
+
+export const BooleanOperation = {
+    None: -1,
+    Union: 0,
+    Subtract: 1,
+    Intersect: 2,
+    Difference: 3
+};
+
+export const ExportOptionsFormat = {
+    PNG: 'png',
+    JPG: 'jpg',
+    TIFF: 'tiff',
+    PDF: 'pdf',
+    EPS: 'eps',
+    SVG: 'svg'
+};
+
+export const BlendingMode = {
+    Normal: 0,
+    Darken: 1,
+    Multiply: 2,
+    ColorBurn: 3,
+    Lighten: 4,
+    Screen: 5,
+    ColorDodge: 6,
+    Overlay: 7,
+    SoftLight: 8,
+    HardLight: 9,
+    Difference: 10,
+    Exclusion: 11,
+    Hue: 12,
+    Saturation: 13,
+    Color: 14,
+    Luminosity: 15
+};
+
+export const TextAlignment = {
+    Left: 0,
+    Right: 1,
+    Center: 2,
+    Justified: 3
+};
+
+export const TextBehaviour = {
+    Auto: 0,
+    Fixed: 1
+};
+
+export const CurvePointMode = {
+    Straight: 1,
+    Mirrored: 2,
+    Disconnected: 4,
+    Asymmetric: 3
+};
+
+export const ModelClasses = {
+    // Base model classes.
+    MSModelObject: 'MSModelObject',
+    MSModelBase: 'MSModelBase',
+    MSDocumentData: 'MSDocumentData',
+
+    // Base layers.
+    MSLayer: 'MSLayer',
+
+    // Core layers.
+    MSShapeGroup: 'MSShapeGroup',
+    MSLayerGroup: 'MSLayerGroup',
+    MSPage: 'MSPage',
+    MSTextLayer: 'MSTextLayer',
+    MSBitmapLayer: 'MSBitmapLayer',
+    MSSliceLayer: 'MSSliceLayer',
+    MSArtboardGroup: 'MSArtboardGroup',
+
+    // Primitive shapes.
+    MSOvalShape: 'MSOvalShape',
+    MSRectangleShape: 'MSRectangleShape',
+    MSStarShape: 'MSStarShape',
+    MSPolygonShape: 'MSPolygonShape',
+    MSTriangleShape: 'MSTriangleShape',
+
+    // Style related.
+    MSStyle: 'MSStyle',
+    MSStyleBorder: 'MSStyleBorder',
+    MSStyleFill: 'MSStyleFill',
+    MSStyleShadow: 'MSStyleShadow',
+    MSStyleInnerShadow: 'MSStyleInnerShadow',
+    MSStyleBlur: 'MSStyleBlur',
+    MSStyleBorderOptions: 'MSStyleBorderOptions',
+    MSStyleColorControls: 'MSStyleColorControls',
+    MSStyleReflection: 'MSStyleReflection'
+};
